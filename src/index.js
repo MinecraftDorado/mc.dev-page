66db11f5d1ad9b00c280799ba2f18d3fcfdf077b
MinecraftDorado <minecraftdorado@gmail.com>
2020-12-31 16:05:16 -0300

    Updates
    » Post updates
    » See all updates

--- src/index.js
+++ src/index.js
@@ -3,16 +3,34 @@ const app = express()
 
 const path = require('path')
 
+const mongoose = require('mongoose')
+const Article = require('./models/article')
+const articleRouter = require('./routes/index')
+const methodOverride = require('method-override')
+
+mongoose.connect('mongodb://localhost/blog', {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+})
+
 // settings
 app.set('port', 3000) // Define una variable que se puede obtener desde otro archivo
 app.set('views', path.join(__dirname, 'views'))
 app.engine('html', require('ejs').renderFile)
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
 
 // routes
 app.use(require('./routes/index'))
 
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.render('index', { title:'BlackMarket', articles: articles })
+})
+
 app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/updates', articleRouter)
 
 // listening the server
 app.listen(app.get('port'), () => {
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,52 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.render('index.html', {title: "Index"})
+const Article = require('./../models/article')
+    
+
+router.get('/new', (req, res) => {
+    res.render('updates/new', { article: new Article() })
 })
 
-router.get('/update', (req, res) => {
-    res.render('updates/content.html', {title: "Update"})
+router.get('/edit/:id', async (req, res) => {
+    const article = await Article.findById(req.params.id)
+    res.render('updates/edit', { article: article })
 })
+
+router.get('/:slug', async (req, res) => {
+    const article = await Article.findOne({ slug: req.params.slug })
+    if (article == null) res.redirect('/')
+    res.render('updates/content', {article: article })
+})
+
+router.post('/', async (req, res, next) => {
+    req.article = new Article()
+    next()
+}, saveArticleAndRedirect('new'))
+
+router.put('/:id', async (req, res, next) => {
+    req.article = await Article.findById(req.params.id)
+    next()
+}, saveArticleAndRedirect('edit'))
+
+router.delete('/:id', async (req, res) => {
+    await Article.findByIdAndDelete(req.params.id)
+    res.redirect('/')
+})
+
+function saveArticleAndRedirect(path) {
+    return async (req, res) => {
+        let article = req.article
+        article.title = req.body.title
+        article.description = req.body.description
+        article.markdown = req.body.markdown
+        try {
+            article = await article.save()
+            res.redirect(`/updates/${article.slug}`)
+        } catch (e) {
+            res.render(`updates/${path}`, { article: article })
+        }
+    }
+}
 
 module.exports = router
